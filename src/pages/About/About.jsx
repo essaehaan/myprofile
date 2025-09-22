ac39c665abe623a28c843c26e202760cc9a1c496
Essa Ehaan <94886958+essaehaan@users.noreply.github.com>
2025-09-23 03:18:10 +0500

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,4 @@
-import HeroImg from "@/assets/images/Essa.png";
+import HeroImg from "@/assets/images/me.webp";
 import OlovaLogo from "@/assets/images/olova.png";
 
 export default function About() {
